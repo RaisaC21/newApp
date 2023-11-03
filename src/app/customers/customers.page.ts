91214b1c15df0087097dd11b9b707e980dc37371
Raisa Chantal <raisae06@gmail.com>
2023-11-03 10:39:13 -0500

    add: desarrrollado hasta el video 17

--- src/app/customers/customers.page.ts
+++ src/app/customers/customers.page.ts
@@ -11,12 +11,16 @@ import { map } from 'rxjs/operators';
 })
 export class CustomersPage implements OnInit {
   users: any = [];
-  permission: boolean=false;
+  permission: boolean = false;
+  searchedUser: any = [];
   constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit() {
     this.permission = true;
-    this.getUsers().subscribe((res) => (this.users = res));
+    this.getUsers().subscribe((res) => {
+      this.users = res;
+      this.searchedUser = this.users;
+    });
   }
   goToHome() {
     this.router.navigate(['/home']);
@@ -25,5 +29,22 @@ export class CustomersPage implements OnInit {
     return this.http
       .get('assets/files/customers.json')
       .pipe(map((res: any) => res.data));
+  }
+
+  searchCustomer(event: any) {
+    const text: string = event.target.value.trim();
+    this.searchedUser = this.users;
+    if (text != '') {
+      this.searchedUser = this.searchedUser.filter((user: any) => {
+        return user.name.toLowerCase().indexOf(text.toLowerCase()) > -1;
+      });
+    }
+  }
+  handleRefresh(event: any) {
+    this.getUsers();
+    setTimeout(() => {
+      // Any calls to load data go here
+      event.target.complete();
+    }, 2000);
   }
 }
